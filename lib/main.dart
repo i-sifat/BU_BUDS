7f33478c204e4daa245b8f55a2e5f3bbd211eb46
i-sifat <murshedalam850@gmail.com>
2024-09-28 00:25:27 +0600

    added sharedpreference to check the app is opened or not

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:bu_buds/screens/home_screenview.dart';
 import 'package:flutter/services.dart';
-
-import 'screens/onboarding_screen/initial_page.dart';
-
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // Import shared_preferences
+import 'screens/onboarding_screen/initial_page.dart';
+// Replace with your actual path
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Ensure the status bar remains transparent
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarBrightness: Brightness.dark,
@@ -13,15 +17,24 @@ void main() {
     systemNavigationBarIconBrightness: Brightness.dark,
     statusBarIconBrightness: Brightness.dark,
   ));
-  WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MyApp());
+  // Check whether this is the first time opening the app
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isFirstTime = prefs.getBool('isFirstTime') ?? true;
+
+  // Update the flag to mark that the app has been opened
+  if (isFirstTime) {
+    prefs.setBool('isFirstTime', false);
+  }
+
+  runApp(MyApp(isFirstTime: isFirstTime));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isFirstTime;
 
-  // This widget is the root of your application.
+  const MyApp({super.key, required this.isFirstTime});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,7 +43,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
         useMaterial3: true,
       ),
-      home: const InitialPageView(),
+      // Show InitialPageView if first time, else show HomeScreenView
+      home: isFirstTime ? const InitialPageView() : const MyHomeScreenView(),
     );
   }
 }
