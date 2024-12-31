cd026f28adcc3f2859d7983cafab1443f6181874
i-sifat <murshedalam850@gmail.com>
2024-12-31 18:49:40 +0600

    some code cleanup

--- lib/navbar_items/menu_navbar.dart
+++ lib/navbar_items/menu_navbar.dart
@@ -1,5 +1,6 @@
 import 'package:bubuds/models/menu_itam.dart';
 import 'package:flutter/material.dart';
+
 import 'package:url_launcher/url_launcher.dart';
 
 class MenuNavigationbarView extends StatelessWidget {
