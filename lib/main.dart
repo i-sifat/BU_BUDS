11ac2adb010b0cae597e4708761002d1a8d8103f
i-sifat <murshedalam850@gmail.com>
2024-12-31 19:59:49 +0600

    corrected some consistency in color

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:bubuds/screens/home_screenview.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart'; // Import shared_preferences
+import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/onboarding_screen/initial_page.dart';
-// Replace with your actual path
+import 'utils/theme.dart';
+import 'utils/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Ensure the status bar remains transparent
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarBrightness: Brightness.dark,
@@ -18,11 +18,9 @@ void main() async {
     statusBarIconBrightness: Brightness.dark,
   ));
 
-  // Check whether this is the first time opening the app
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool isFirstTime = prefs.getBool('isFirstTime') ?? true;
 
-  // Update the flag to mark that the app has been opened
   if (isFirstTime) {
     prefs.setBool('isFirstTime', false);
   }
@@ -39,11 +37,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
-        useMaterial3: true,
-      ),
-      // Show InitialPageView if first time, else show HomeScreenView
+      theme: AppTheme.lightTheme,
       home: isFirstTime ? const InitialPageView() : const MyHomeScreenView(),
     );
   }
